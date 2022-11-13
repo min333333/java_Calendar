215152641ba1b697565d06c879bc199281e35762
Min <ahffk0962@naver.com>
2022-11-13 20:24:23 +0900

    print calendar (auto)

--- src/honux/calandar/Prompt.java
+++ src/honux/calandar/Prompt.java
@@ -30,7 +30,6 @@ public class Prompt {
 
 		int year = 1;
 		int month = 1;
-		int weekday = 1;
 
 		while (true) {
 			System.out.println("년도를 입력하세요.");
@@ -48,11 +47,7 @@ public class Prompt {
 			if (month > 12 || month < -1) {
 				continue;
 			}
-			System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
-			System.out.print(PROMPT);
-			String Str_weekday = scan.next();
-			weekday = parseDay(Str_weekday);
-			cal.printCalendar(year, month, weekday);
+			cal.printCalendar(year, month);
 		}
 		System.out.println("gg");
 		scan.close();
